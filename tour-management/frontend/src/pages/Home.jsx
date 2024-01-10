1a8df5c975dbccba70e793411202ea3ced964b98
Danmok01 <rodrigomarquespf@gmail.com>
2024-01-10 12:51:55 -0300

    experience section

--- tour-management/frontend/src/pages/Home.jsx
+++ tour-management/frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import '../styles/home.css'
+import '../styles/home.css';
 
 import {Container, Row, Col} from 'reactstrap';
-import heroImg from '../assets/images/hero-img01.jpg'
-import heroImg02 from '../assets/images/hero-img02.jpg'
-import heroVideo from '../assets/images/hero-video.mp4'
-import worldImg from './../assets/images/world.png'
+import heroImg from '../assets/images/hero-img01.jpg';
+import heroImg02 from '../assets/images/hero-img02.jpg';
+import heroVideo from '../assets/images/hero-video.mp4';
+import worldImg from './../assets/images/world.png';
+import experienceImg from './../assets/images/experience.png';
+
 import Subtitle from './../shared/Subtitle';
 
 import SearchBar from "../shared/SearchBar";
@@ -81,6 +83,46 @@ const Home = () => {
     </Container>
   </section>
   {/*  feature tour section fim*/}
+
+  {/*Experience section*/}
+  <section>
+    <Container>
+      <Row>
+        <Col lg='6'>
+          <div className="experience__content">
+            <Subtitle subtitle={'Experience'}/>
+              <h2>With our all experience <br/> we will serve you</h2>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              <br/>
+              Lorem ipsum dolor sit amet, consectetur adipiscing.
+              </p>
+          </div>
+          <div className="counter__wrapper d-flex align-items-center gap-5">
+            <div className="counter__box">
+              <span>12k+</span>
+              <h6>Successfull Trip</h6>
+            </div>
+            <div className="counter__box">
+              <span>2k+</span>
+              <h6>Regular clients</h6>
+            </div>
+            <div className="counter__box">
+              <span>15</span>
+              <h6>Years experience</h6>
+            </div>
+          </div>
+        </Col>
+        <Col lg='6'>
+          <div className="experience__img">
+            <img src={experienceImg} alt="" />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  </section>
+
+  {/*Experience section fim*/}
 
   </>
 };
