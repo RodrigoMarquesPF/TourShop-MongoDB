aa619f469884f94de7330df839cc93814589a001
Danmok01 <rodrigomarquespf@gmail.com>
2024-01-09 23:23:11 -0300

    Page Home - Search - Header

--- tour-management/frontend/src/router/Routers.js
+++ tour-management/frontend/src/router/Routers.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import {Routes, Route, Navigate} from 'react-router-dom'
 
-import Home from './../pages/Home';
-import Tours from './../pages/Tours';
-import Login from './../pages/Login';
-import Register from './../pages/Register';
-import SearchResultList from './../pages/SearchResultList';
-import ToursDetails from './../pages/ToursDetails';
+import Home from '../pages/Home';
+import Tours from '../pages/Tours';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import SearchResultList from '../pages/SearchResultList';
+import ToursDetails from '../pages/ToursDetails';
 
-const Router = () => {
+const Routers = () => {
   return (
     <Routes>
         <Route path="/" element={<Navigate to="/home"/>}/>
@@ -22,4 +22,4 @@ const Router = () => {
   )
 }
 
-export default Router
+export default Routers;
