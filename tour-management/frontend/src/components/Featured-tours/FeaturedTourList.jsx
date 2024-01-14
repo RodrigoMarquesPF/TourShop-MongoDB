2e965dc0379f67f50a115da2e7260b6c011c2351
RodrigoMarquesPF <rodrigomarquespf@gmail.com>
2024-01-14 20:38:47 -0300

    responsive

--- tour-management/frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ tour-management/frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -24,7 +24,7 @@ const FeaturedTourList = () => {
       
     }
   {!loading && featuredTours?.map(tour =>(
-        <Col lg='3' className='mb-4' key={tour._id}>
+        <Col lg='3' md='4' sm='6' className='mb-4' key={tour._id}>
             <TourCard tour={tour}/>
             </Col>
     ))
--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -25,7 +25,8 @@ const nav__links=[
 
 const Header = () => {
 
-  const headerRef =useRef(null)
+  const headerRef =useRef(null);
+  const menuRef =useRef(null);
   const navigate = useNavigate()
   const {user, dispatch} = useContext(AuthContext)
 
@@ -50,6 +51,8 @@ const Header = () => {
     return window.removeEventListener('scroll',stickyHeaderFunc)
   })
 
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+
   return  (<header className="header" ref={headerRef}>
     <Container>
       <Row>
@@ -60,7 +63,7 @@ const Header = () => {
           </div>
           {/*fim logo*/}
           {/*Menu*/}
-          <div className="navigation">
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
             <ul className="menu d-flex align-items-center gap-5">
               {
                 nav__links.map((item, index)=>(
@@ -91,7 +94,7 @@ const Header = () => {
 
             </div>
 
-            <span className="mobile__menu">
+            <span className="mobile__menu" onClick={toggleMenu}>
               <i class="ri-menu-line"></i>
             </span>
           </div>
